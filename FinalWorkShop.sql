b8ccc8ac86c9856ed20f3c54dc683e4ec37c23fe
mihajlovaskov <mihajlovaskov@gmail.com>
2022-06-06 22:49:34 +0200

    Update FinalWorkShop.sql

--- FinalWorkShop.sql
+++ FinalWorkShop.sql
@@ -47,7 +47,7 @@ Foreign key(SizeId)
 References Size (Id);
 
 GO
-CREATE OR ALTER FUNCTION dbo.fn_MergeFirstNameAndLastName(@firstName nvarchar (20), @lastName nvarchar(20) 
+CREATE OR ALTER FUNCTION dbo.fn_MergeFirstNameAndLastName(@firstName nvarchar (20), @lastName nvarchar(20)) 
 RETURNS nvarchar (2000)
 AS
 BEGIN
@@ -60,7 +60,9 @@ RETURN @result
 END
 
 SELECT dbo.fn_MergeFirstNameAndLastName('Darko','Nikolikj') as FullName;
---There should be a view to show all pizzas that are not delivered yet --and the full names of the users waiting for them
+--There should be a view to show all pizzas that are not delivered yet 
+--and the full names of the users waiting for them
+
 CREATE VIEW vw_MyView 
 AS (
 SELECT Pizza.Name , dbo.fn_MergeFirstNameAndLastName([User].FirstName,[User].LastName) as FullName, IsDelivered
@@ -99,4 +101,32 @@ End
 EXEC InsertValues;
 
 
---There should be a stored procedure that when provided an Order Id, --can return the full price of a whole order ( Delivery + All Pizza + All Toppings )GOCREATE or ALTER Procedure FullPrice(@OrderId int, @delivery int)as BeginSELECT  SUM(@delivery + Pizza.Price + Topping.Price) as  TotalPriceFROM Pizza INNER JOIN PizzaTopings on PizzaId = Pizza.IdINNER JOIN Topping on ToppingId = Topping.IdWHERE Pizza.OrderId = @OrderIdEndCREATE TABLE PizzaTopings ( Id int Primary Key Identity NOT NULL,ToppingId int Foreign Key References Topping(Id),PizzaId int Foreign Key References Pizza(Id));INSERT INTO PizzaTopingsVALUES(1,1);EXEC FullPrice @OrderId = 1 , @delivery = 20 ;
+--There should be a stored procedure that when provided an Order Id, 
+--can return the full price of a whole order ( Delivery + All Pizza + All Toppings )
+GO
+CREATE or ALTER Procedure FullPrice(
+
+@OrderId int, 
+@delivery int
+)
+
+as 
+Begin
+SELECT  SUM(@delivery + Pizza.Price + Topping.Price) as  TotalPrice
+FROM Pizza 
+INNER JOIN PizzaTopings on PizzaId = Pizza.Id
+INNER JOIN Topping on ToppingId = Topping.Id
+WHERE Pizza.OrderId = @OrderId
+
+End
+
+CREATE TABLE PizzaTopings ( 
+Id int Primary Key Identity NOT NULL,
+ToppingId int Foreign Key References Topping(Id),
+PizzaId int Foreign Key References Pizza(Id)
+);
+
+INSERT INTO PizzaTopings
+VALUES(1,1);
+
+EXEC FullPrice @OrderId = 1 , @delivery = 20 ;
